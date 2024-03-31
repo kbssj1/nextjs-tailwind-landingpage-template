b6b5a4bd7a5b9aec519b2886bb21df887e92906f
kbssj1 <kbssj1@naver.com>
2024-03-31 17:07:37 +0900

    mobile responsive

--- src/app/components/Advantages.tsx
+++ src/app/components/Advantages.tsx
@@ -6,38 +6,38 @@ const Advantages = () => {
 
     <p className="text-xl text-center py-8">Product Advantage</p>
 
-    <div className="background-color: rgb(156 163 175) h-400 flex gap-4">
+    <div className="background-color: rgb(156 163 175) h-400 flex max-sm:flex-col gap-4">
       
       <div className="rounded overflow-hidden shadow-lg basis-1/3 text-center">
         <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">
-          Save 80%
-        </div>
-        <p className="text-gray-700 text-base">
-          Create a game with this template and save your time. This template has a lot. 
-        </p>
-        </div>
-      </div>
-
-      <div className="rounded overflow-hidden shadow-lg basis-1/3 text-center">
-        <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">
-          Save 80%
-        </div>
-        <p className="text-gray-700 text-base">
-          Create a game with this template and save your time. This template has a lot. 
-        </p>
+          <div className="font-bold text-xl mb-2">
+            Save 80%
+          </div>
+          <p className="text-gray-700 text-base">
+            Create a game with this template and save your time. This template has a lot. 
+          </p>
         </div>
       </div>
 
       <div className="rounded overflow-hidden shadow-lg basis-1/3 text-center">
         <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">
-          Save 40%
+          <div className="font-bold text-xl mb-2">
+            Save 80%
+          </div>
+          <p className="text-gray-700 text-base">
+            Create a game with this template and save your time. This template has a lot. 
+          </p>
         </div>
-        <p className="text-gray-700 text-base">
-          We sell direct to save 40% No Hidden fees
-        </p>
+      </div>
+
+      <div className="rounded overflow-hidden shadow-lg basis-1/3 text-center">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">
+            Save 40%
+          </div>
+          <p className="text-gray-700 text-base">
+            We sell direct to save 40% No Hidden fees
+          </p>
         </div>
       </div>
 
--- src/app/components/Features.tsx
+++ src/app/components/Features.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./Button";
 
 const Features = () => {
 
@@ -7,14 +8,21 @@ const Features = () => {
         <h3 className="text-blue-500 text-center text-6xl">Best Product <br></br> For Your Digital Game</h3>
         <br/>
         <p className="text-xl text-center py-10">Get an Awesome Game Template <br></br> For Your Time</p>
-        <div className="aspect-video">
-          <iframe
-            className="w-[600px] h-[300px]"
-            src="https://www.youtube.com/embed/9bGwq0IBGT4?si=N3ZImZdvLCYEstEw"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          >
-          </iframe>
+        <div className="hidden sm:block">
+          <div className="aspect-video">
+            <iframe
+              className="w-[600px] h-[300px]"
+              src="https://www.youtube.com/embed/9bGwq0IBGT4?si=N3ZImZdvLCYEstEw"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            >
+            </iframe>
+          </div>
         </div>
+
+        <div className="hidden max-sm:block">
+          <Button title="Action" />
+        </div>
+
       </div>
     </div>
   );
