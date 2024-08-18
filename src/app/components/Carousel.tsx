9c6c5a228c972aa40c2803b578e19e8bfd42f68a
kbssj1 <kbssj1@naver.com>
2024-08-18 14:16:53 +0900

    add props

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -1,35 +1,41 @@
 'use client';
 
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, PropsWithChildren } from 'react'
 
-const Carousel = ({ children:slides, autoSlide = false, autoSlideInterval = 3000 }) => {
-    const [curr, setCurr] = useState(0);
-    const prev = () => setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1));
-    const next = () => setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1));
-    const click = (i:number) => {setCurr(i)};
+type props = {
+  autoSlide: boolean,
+  autoSlideInterval: number,
+  children: React.ReactElement[]
+}
 
-    useEffect(() => {
-        if (!autoSlide) return;
-        const slideInterval = setInterval(next, autoSlideInterval);
-        return () => clearInterval(slideInterval);
-    }, [])
+const Carousel = ({ children, autoSlide = false, autoSlideInterval = 3000 } : props) => {
+  const [curr, setCurr] = useState(0);
+  const prev = () => setCurr((curr) => (curr === 0 ? children.length - 1 : curr - 1));
+  const next = () => setCurr((curr) => (curr === children.length - 1 ? 0 : curr + 1));
+  const click = (i:number) => {setCurr(i)};
 
-    return (
-      <div className='w-full'>
-        <div className='overflow-hidden relative'>
-          <div className='flex transition-transform ease-out duration-500' style={{ transform: `translateX(-${curr * 100}%)` }}>
-            {slides}
-          </div>
-          <div className='absolute bottom-4 right-0 left-0'>
-            <div className='flex items-center justify-center gap-2'>
-              {slides.map((s, i) => (
-                <div key={i} onClick={()=>click(i)} className={`transition-all w-3 h-3 bg-white rounded-full  ${curr === i ? "p-0.5" : "bg-opacity-50"}`} />
-              ))}
-            </div>
+  useEffect(() => {
+      if (!autoSlide) return;
+      const slideInterval = setInterval(next, autoSlideInterval);
+      return () => clearInterval(slideInterval);
+  }, [])
+
+  return (
+    <div className='w-full'>
+      <div className='overflow-hidden relative'>
+        <div className='flex transition-transform ease-out duration-500' style={{ transform: `translateX(-${curr * 100}%)` }}>
+          {children}
+        </div>
+        <div className='absolute bottom-4 right-0 left-0'>
+          <div className='flex items-center justify-center gap-2'>
+            {children.map((s, i) => (
+              <div key={i} onClick={()=>click(i)} className={`transition-all w-3 h-3 bg-white rounded-full  ${curr === i ? "p-0.5" : "bg-opacity-50"}`} />
+            ))}
           </div>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Carousel
--- src/app/page2/page.tsx
+++ src/app/page2/page.tsx
@@ -6,7 +6,7 @@ export default function Page2() {
   return (
     <>
       <Navbar />
-      <Carousel>
+      <Carousel autoSlide={false} autoSlideInterval={0}>
         <Image src="/digital.jpg" unoptimized width={0} height={0} sizes="100vw" style={{ minWidth: '100%', height: '700px' }} alt={'image'} />
         <Image src="/code.png" unoptimized width={0} height={0} sizes="100vw" style={{ minWidth: '100%', height: '700px' }} alt={'image'} />
       </Carousel>
